981282abfe3575641d2791bfc352e543461bcb7e
dhajni-okt <dezso.hajnalka@bolyaimovar.com>
2021-09-12 10:55:34 +0200

    3.-4. Óra

--- 3-4 óra/elso.py
+++ 3-4 óra/elso.py
@@ -3,7 +3,6 @@
 print("Hello, World!")
 print()
 print("Szorgalmasan gyakorolni\nfogom a programozást!")
-print("Minten házi feladatot \r időre elkészítek")
 print("Csak ötöseim *\b lesznek.")
 print("\110\141\152\156\151")
 print("\x70\x79\x74\x68\x6F\x6E")
